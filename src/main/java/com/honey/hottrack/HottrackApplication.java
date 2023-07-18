322902353a98bbd64ff5cd7297f0f102a34124b2
hingjae <ljh23270@naver.com>
2023-07-18 16:02:08 +0900

    first commit

---
+++ src/main/java/com/honey/hottrack/HottrackApplication.java
@@ -0,0 +1,13 @@
+package com.honey.hottrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HottrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HottrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honey/hottrack/HottrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honey.hottrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HottrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
